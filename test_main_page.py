abe3ac7625add0341c56854caa156b9a2151a4a3
kinteriq <kinteriq@gmail.com>
2019-10-01 18:51:44 +0500

    Finish test with login link.

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,15 @@
 from pages.main_page import MainPage
 
 
+LINKS = [
+    'http://selenium1py.pythonanywhere.com/',
+    'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer',
+]
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
+    page.should_be_login_link()
     page.go_to_login_page()
