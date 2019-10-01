cc2edc1e1b7789960c0ae51df4c2e860ff905e27
kinteriq <kinteriq@gmail.com>
2019-10-01 19:06:58 +0500

    Add LOGIN_LINK to locators.py; Create login_page.py.

---
+++ pages/locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    """
+    BasePage.is_element_present(how, what)
+    """
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
