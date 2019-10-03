03e4007e2c6a1fc88d5d7e4640aa23669d5782a4
kinteriq <kinteriq@gmail.com>
2019-10-03 22:17:51 +0500

    Add test login link.

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,7 +3,7 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    def get_message(self, *args):
+    def _get_message(self, *args):
         self.browser.implicitly_wait(10)
         return self.browser.find_element(*args).text
 
@@ -13,18 +13,21 @@ class ProductPage(BasePage):
     
     def correct_added_to_busket_book(self):
         title = self.browser.find_element(*ProductPageLocators.TITLE).text
-        assert title == self.get_message(*ProductPageLocators.ADDED_TITLE),\
+        assert title == self._get_message(*ProductPageLocators.ADDED_TITLE),\
             "The title of added to basket book is incorrect"
 
     def correct_added_to_busket_price(self):
         price = self.browser.find_element(*ProductPageLocators.PRICE).text
-        assert price == self.get_message(*ProductPageLocators.TOTAL_PRICE),\
+        assert price == self._get_message(*ProductPageLocators.TOTAL_PRICE),\
             "The price of added to basket book is incorrect"
-
-    def should_not_be_success_message(self):
-        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
-            "Success message is present, but should not be"
     
+    def should_be_login_link(self):
+        pass
+
     def success_message_should_dissapear(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message did not dissapear"
+    
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is present, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,10 @@ import pytest
 from pages.product_page import ProductPage
 
 
-LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019.'
+LINKS = [
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019.',
+    "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/",
+]
 
 
 @pytest.mark.parametrize('link', [
@@ -31,10 +34,10 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.correct_added_to_busket_price()
 
 
-@pytest.mark.xfail(strict=True)
 @pytest.mark.success_msg
+@pytest.mark.xfail(strict=True)
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, LINK)
+    page = ProductPage(browser, LINKS[0])
     page.open()
 
     page.add_to_basket_click()
@@ -45,18 +48,32 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 
 @pytest.mark.success_msg
 def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, LINK)
+    page = ProductPage(browser, LINKS[0])
     page.open()
     page.should_not_be_success_message()
 
 
-@pytest.mark.xfail(strict=True)
 @pytest.mark.success_msg
+@pytest.mark.xfail(strict=True)
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, LINK)
+    page = ProductPage(browser, LINKS[0])
     page.open()
 
     page.add_to_basket_click()
     page.solve_quiz_and_get_code()
 
     page.success_message_should_dissapear()
+
+
+@pytest.mark.login
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, LINKS[1])
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.login
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, LINKS[1])
+    page.open()
+    page.go_to_login_page()
